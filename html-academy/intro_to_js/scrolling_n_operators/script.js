14fb760d80e7f772eae2dbd2a49906cdd1dd3385
Ansagan-B <ansagan.gb@gmail.com>
2021-07-09 15:08:24 +0300

    website evaluation

--- html-academy/intro_to_js/scrolling_n_operators/script.js
+++ html-academy/intro_to_js/scrolling_n_operators/script.js
@@ -1,32 +1,22 @@
-let list = document.querySelector('.todo-list');
-let input = document.querySelector('.todo-input');
-let form = document.querySelector('.todo-form');
-let priority = document.querySelector('.todo-priority');
+let review = document.querySelectorAll('.review');
+let submitButton = document.querySelector('.submit-button');
+let error = document.querySelector('.error');
 
-priority.onclick = function () {
-    priority.classList.toggle('is-important');
-    if (priority.classList.contains('is-important')) {
-        priority.textContent = 'Важная задача';
-    } else {
-        priority.textContent = 'Обычная задача';
+for (let i of review) {
+    i.onchange = function() {
+        if (i.dataset.evaluation === 'good') {
+            submitButton.disabled = false;
+            error.classList.remove('shown')
+        } else if (i.dataset.evaluation === 'bad'){
+            submitButton.disabled = true;
+            error.classList.add('shown')
+        }
     }
-};
-
-form.onsubmit = function (evt) {
-    evt.preventDefault();
-    let newTask = document.createElement('li');
-    newTask.textContent = input.value;
-
-    if (priority.classList.contains('is-important')) {
-        newTask.classList.add('is-important')
-    }
-    input.value = '';
-    list.append(newTask);
-};
-
+}
 /*
-1. Каждая задача в списке — это элемент li. При отправке формы (переменная form) новая задача добавляется в конец списка (переменная list).
-2. Текст задачи берётся из поля ввода (переменная input).
-3. Если у переключателя приоритета (переменная priority) есть класс is-important, то новой задаче также добавляется класс is-important.
-4. Бонус: после того, как задача добавится в список, поле ввода можно очистить. Но можно не очищать. Подходят оба варианта.
+1. У всех радиокнопок есть класс review.
+2. Чтобы отслеживать переключение радиокнопок, нужно добавить обработчик событий onchange каждой радиокнопке.
+3. У каждой радиокнопки есть атрибут data-evaluation. Если отзыв хороший, значение этого атрибута – 'good', а если плохой - 'bad'.
+4. Кнопка отправки имеет класс submit-button. Если пользователь выбрал плохой отзыв, кнопку нужно заблокировать, а если хороший — разблокировать.
+5. Чтобы показать сигнал об ошибке, элементу с классом error нужно добавить класс shown. Сигнал нужно показывать, если пользователь выбрал плохой отзыв. Если выбран хороший отзыв, сигнал об ошибке нужно спрятать.
 */
